40292034026001c1d347164c05299d9e41793a44
marlonmmalgas-oss <marlonmmalgas@gmail.com>
2025-08-21 14:59:19 +0200

    Add files via upload

--- Report_Hourly.py
+++ Report_Hourly.py
@@ -176,8 +176,7 @@ Remain     {remaining_restow_load:>5}      {remaining_restow_disch:>5}
 _________________________
 *Hatch Moves*
            Open   Close
-FWD        {hatch_fwd_open:>5}      {hatch_fwd_close:
-	:>5}
+FWD        {hatch_fwd_open:>5}      {hatch_fwd_close:>5}
 MID        {hatch_mid_open:>5}      {hatch_mid_close:>5}
 AFT        {hatch_aft_open:>5}      {hatch_aft_close:>5}
 _________________________
